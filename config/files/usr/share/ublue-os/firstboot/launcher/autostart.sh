1f6a436c6f078625bb2f9e224ab7fe7cb763bf6f
Aki <80670461+akippnn@users.noreply.github.com>
2023-12-26 17:52:22 +0800

    fix: attempt #2 to setting ~/.config/hypr/hyprland.conf as blank

--- config/files/usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ config/files/usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+/usr/share/ublue-os/firstboot/launcher/init-user-hyprland-conf.sh
+
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
 /usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
 
-exec /usr/share/ublue-os/firstboot/init-hyprland-conf.sh
