fc81b6029a03888596104448cb8d6edbdb53da97
Aki <80670461+akippnn@users.noreply.github.com>
2023-12-28 21:52:37 +0800

    feat: removed init-useer-hyprland-conf.sh

--- config/files/usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ config/files/usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/share/ublue-os/firstboot/launcher/init-user-hyprland-conf.sh
-
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
 /usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
 
